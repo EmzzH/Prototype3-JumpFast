40e09ad70d2f8c83023230a86e714d9f1dd9b4d6
EmzzH <145970013+EmzzH@users.noreply.github.com>
2023-10-25 15:03:00 +0100

    fvs

--- Prototype3-JumpFast/Assets/Scripts/PayerController.cs
+++ Prototype3-JumpFast/Assets/Scripts/PayerController.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class PayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
+    private AudioSource playerAudio;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        playerAnim = GetComponent<Animator>();
         playerRb = GetComponent<Rigidbody>();
+        playerAudio = GetComponent<AudioSource>();
+
         Physics.gravity *= gravityModifier;
         
     }
@@ -20,26 +31,36 @@ public class PayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
-
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
 
     }
     private void OnCollisionEnter(Collision collision)
     {
-        
 
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+            explosionParticle.Play();
+            dirtParticle.Stop();
             gameOver = true;
-            Debug.Log("Game Over!");
+            Debug.Log("Game Over!"); 
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+           
+            
+
         }
 
     }
---
+++ Prototype3-JumpFast/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,89 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void MoveLeft_Start_mC7C29FBF538971ED37B9C3A33A5C61C3F64BB30C (void);
+extern void MoveLeft_Update_mC38B78A98EA1362EA0B82B703440FC57EB0E098F (void);
+extern void MoveLeft__ctor_mAE79A11856F61F384C0A556E74280892922F0A97 (void);
+extern void PayerController_Start_m600E19517F814A8E2CCCD61B7D6803D16829CF9D (void);
+extern void PayerController_Update_m59398FD603EBD00CD67106964C985EF9DBFA0AEB (void);
+extern void PayerController_OnCollisionEnter_m557842BA87FD78C3CFFA23AD6293EF0254D813A3 (void);
+extern void PayerController__ctor_mCAEE9B115EBEB1EEECC92DB93593E85AFE7DF050 (void);
+extern void RepeatBackground_Start_mBC99B8939FC946126F5193638EB14ED609C83444 (void);
+extern void RepeatBackground_Update_mBE6CFA1BD94753B2F21E5B461BBB037DE137E20E (void);
+extern void RepeatBackground__ctor_m180B6610D044C7CF19CFD56EDDE6C86995DCFD76 (void);
+extern void SpawnManager_Start_m65C9EA67649948222CFF4FBBF77BE2319D813DAF (void);
+extern void SpawnManager_Update_mD714BA3EADCC182FB7A93B9DE347D727E3A1BC53 (void);
+extern void SpawnManager_SpawnObstacle_mE5D7865D1E72DF72FAB39ACE07DA8AF8E81B4F42 (void);
+extern void SpawnManager__ctor_m8DD503A0FFE79FA38CF0B7F013E54D24A04D166A (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[16] = 
+{
+	MoveLeft_Start_mC7C29FBF538971ED37B9C3A33A5C61C3F64BB30C,
+	MoveLeft_Update_mC38B78A98EA1362EA0B82B703440FC57EB0E098F,
+	MoveLeft__ctor_mAE79A11856F61F384C0A556E74280892922F0A97,
+	PayerController_Start_m600E19517F814A8E2CCCD61B7D6803D16829CF9D,
+	PayerController_Update_m59398FD603EBD00CD67106964C985EF9DBFA0AEB,
+	PayerController_OnCollisionEnter_m557842BA87FD78C3CFFA23AD6293EF0254D813A3,
+	PayerController__ctor_mCAEE9B115EBEB1EEECC92DB93593E85AFE7DF050,
+	RepeatBackground_Start_mBC99B8939FC946126F5193638EB14ED609C83444,
+	RepeatBackground_Update_mBE6CFA1BD94753B2F21E5B461BBB037DE137E20E,
+	RepeatBackground__ctor_m180B6610D044C7CF19CFD56EDDE6C86995DCFD76,
+	SpawnManager_Start_m65C9EA67649948222CFF4FBBF77BE2319D813DAF,
+	SpawnManager_Update_mD714BA3EADCC182FB7A93B9DE347D727E3A1BC53,
+	SpawnManager_SpawnObstacle_mE5D7865D1E72DF72FAB39ACE07DA8AF8E81B4F42,
+	SpawnManager__ctor_m8DD503A0FFE79FA38CF0B7F013E54D24A04D166A,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[16] = 
+{
+	1129,
+	1129,
+	1129,
+	1129,
+	1129,
+	956,
+	1129,
+	1129,
+	1129,
+	1129,
+	1129,
+	1129,
+	1129,
+	1129,
+	2168,
+	1129,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	16,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
